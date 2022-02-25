2bc8c5d77eba0f29e90a2a109be37d449827f93f
mustela <ejankowski@gmail.com>
2022-02-25 21:03:15 +0100

    Fixing

--- lib/seeds/yaml_seed.ex
+++ lib/seeds/yaml_seed.ex
@@ -1,5 +1,6 @@
 defmodule LiveSup.Seeds.YamlSeed do
-  alias LiveSup.Core.Projects
+  alias LiveSup.Core.{Projects, Dashboards}
+  alias LiveSup.Schemas.Project
 
   def seed(data) do
     data
@@ -14,8 +15,19 @@ defmodule LiveSup.Seeds.YamlSeed do
     |> Enum.each(fn project_attrs ->
       project_attrs
       |> Projects.create_public_project()
+      |> import_dashboards(project_attrs)
     end)
   end
+
+  defp import_dashboards(%Project{} = project, %{"dashboards" => dashboards}) do
+    dashboards
+    |> Enum.each(fn dashboard_attrs ->
+      project
+      |> Dashboards.create(dashboard_attrs)
+    end)
+  end
+
+  defp import_dashboards(project, _), do: nil
 
   defp parse_yaml(data) do
     {:ok, parsed_data} = YamlElixir.read_from_string(data)
