aa970def61c64c65f686016f611a156d7a1bd76f
mustela <ejankowski@gmail.com>
2022-02-26 23:33:38 +0100

    Adding widgets

--- lib/seeds/yaml_seed.ex
+++ lib/seeds/yaml_seed.ex
@@ -1,5 +1,5 @@
 defmodule LiveSup.Seeds.YamlSeed do
-  alias LiveSup.Core.{Projects, Dashboards}
+  alias LiveSup.Core.{Projects, Dashboards, Datasources, Widgets}
   alias LiveSup.Schemas.Project
 
   def seed(data) do
@@ -28,6 +28,7 @@ defmodule LiveSup.Seeds.YamlSeed do
     |> Enum.each(fn dashboard_attrs ->
       project
       |> get_or_create_dashboard(dashboard_attrs)
+      |> import_widget(dashboard_attrs)
     end)
   end
 
@@ -36,6 +37,25 @@ defmodule LiveSup.Seeds.YamlSeed do
       {:error, :not_found} -> Dashboards.create(project, attrs)
       {:ok, dashboard} -> {:ok, dashboard}
     end
+  end
+
+  def import_widget({:ok, dashboard}, %{"widgets" => widgets}) do
+    widgets
+    |> Enum.each(fn widget_attrs ->
+      datasource = Datasources.get_by_slug!(widget_attrs["datasource_slug"])
+      {:ok, datasource_instance} = Datasources.create_instance(datasource)
+      widget = Widgets.get_by_slug!(widget_attrs["widget_slug"])
+
+      {:ok, widget_instance} =
+        Widgets.create_instance(
+          widget,
+          datasource_instance,
+          widget_attrs["settings"]
+        )
+
+      dashboard
+      |> Dashboards.add_widget(widget_instance)
+    end)
   end
 
   # defp import_dashboards(project, _), do: nil
--- test/seeds/yaml_seed_test.exs
+++ test/seeds/yaml_seed_test.exs
@@ -4,12 +4,12 @@ defmodule LiveSup.Test.Seeds.YamlSeed do
 
   import LiveSup.Test.Setups
 
-  alias LiveSup.Core.{Projects, Dashboards}
+  alias LiveSup.Core.{Projects, Dashboards, Widgets, Datasources}
 
   describe "Seed from a yaml file" do
     @describetag :yaml_seed
 
-    setup [:setup_groups]
+    setup [:setup_groups, :weather_widget_fixture]
 
     test "data is imported" do
       LiveSup.Seeds.YamlSeed.seed(yaml_data())
@@ -21,29 +21,90 @@ defmodule LiveSup.Test.Seeds.YamlSeed do
 
       assert length(project.dashboards) == 2
 
-      dashboard = Dashboards.get!("469430b6-d754-497d-988e-34079faafd12")
-      assert %{name: "Dashboard 2"} = dashboard
+      dashboard = Dashboards.get!("af65c472-40cb-4824-8224-708cec8806de")
+      assert %{name: "Dashboard 1"} = dashboard
+
+      widget_instances = Dashboards.widgets_instances(dashboard)
+      assert length(widget_instances) == 1
     end
 
     defp yaml_data do
       """
       projects:
-        - id: 5727d4e3-b3d4-460c-a3fb-f0180d5c3777
-          name: "Awesome Project"
-          slug: "awesome-project"
-          internal: false
-          default: false
+        -
           avatar_url: "https://awesome.com/my-awesome-project.jpeg"
           dashboards:
-            - id: af65c472-40cb-4824-8224-708cec8806de
+            -
+              default: false
+              id: af65c472-40cb-4824-8224-708cec8806de
               name: "Dashboard 1"
+              widgets:
+                -
+                  datasource_slug: weather-api-datasource
+                  widget_slug: weather
+                  enabled: true
+                  feature_image_url: /images/widgets/weather.png
+                  global: false
+                  labels: []
+                  name: Weather
+                  settings:
+                    location:
+                      source: local
+                      type: string
+                      value: ""
+                    runs_every:
+                      source: local
+                      type: int
+                      value: 43200
+                  slug: weather
+                  ui_handler: LiveSupWeb.Live.Widgets.WeatherLive"
+                  worker_handler: LiveSup.Core.Widgets.Weather.Worker
+            -
               default: false
-              widgets: []
-            - id: 469430b6-d754-497d-988e-34079faafd12
+              id: 469430b6-d754-497d-988e-34079faafd12
               name: "Dashboard 2"
-              default: false
               widgets: []
+          default: false
+          id: 5727d4e3-b3d4-460c-a3fb-f0180d5c3777
+          internal: false
+          name: "Awesome Project"
+          slug: awesome-project
+
       """
     end
+  end
+
+  def weather_widget_fixture(_) do
+    %{
+      name: "Weather API Datasource",
+      slug: "weather-api-datasource",
+      enabled: true,
+      labels: [],
+      settings: %{"api_key" => nil}
+    }
+    |> Datasources.create()
+    |> weather_widget()
+  end
+
+  def weather_widget({:ok, datasource}) do
+    %{
+      name: "Weather",
+      slug: "weather",
+      enabled: true,
+      global: false,
+      feature_image_url: "/images/widgets/weather.png",
+      ui_handler: "LiveSupWeb.Live.Widgets.WeatherLive",
+      worker_handler: "LiveSup.Core.Widgets.Weather.Worker",
+      labels: [],
+      settings: %{
+        # 12 hours
+        "runs_every" => %{"source" => "local", "type" => "int", "value" => 43200},
+        "location" => %{"source" => "local", "type" => "string", "value" => ""}
+      },
+      datasource_id: datasource.id
+    }
+    |> Widgets.create!()
+
+    :ok
   end
 end
