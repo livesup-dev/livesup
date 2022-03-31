9dd5014bf347ccef0b6b2b789109bf85b00a000e
mustela <ejankowski@gmail.com>
2022-04-01 00:02:47 +0200

    Changing settings again

--- config/config.exs
+++ config/config.exs
@@ -74,8 +74,8 @@ config :ueberauth, Ueberauth.Strategy.Google.OAuth,
   client_secret: {System, :get_env, ["GOOGLE_OAUTH_CLIENT_SECRET"]}
 
 config :ueberauth, Ueberauth.Strategy.Github.OAuth,
-  client_id: {System, :get_env, ["GITHUB_OAUTH_CLIENT_ID"]},
-  client_secret: {System, :get_env, ["GITHUB_OAUTH_CLIENT_SECRET"]}
+  client_id: {:system, "GITHUB_OAUTH_CLIENT_ID"},
+  client_secret: {:system, "GITHUB_OAUTH_CLIENT_SECRET"}
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
