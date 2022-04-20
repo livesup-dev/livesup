4c1b566d4bbe3251bf67167fa1566672268e7043
Memo <guillermo@dinkuminteractive.com>
2022-04-20 10:02:55 -0300

    Fix team and on call widget

--- lib/live_sup_web/live/widgets/pager_duty/on_call_live.ex
+++ lib/live_sup_web/live/widgets/pager_duty/on_call_live.ex
@@ -8,14 +8,14 @@ defmodule LiveSupWeb.Live.Widgets.PagerDuty.OnCallLive do
       <!-- Metrics Goal -->
       <.live_component module={WidgetHeaderComponent} id={"#{widget_data.id}-header"} widget_data={widget_data} />
       <!-- Widget Content -->
-      <div class="p-2 grid justify-items-center">
+      <div class="p-2 grid justify-items-center min-h-[132px]">
 
         <%= for on_calls <- widget_data.data do %>
 
-          <div class="card w-96 hover:shadow-none relative flex flex-col mx-auto shadow-lg m-5">
-            <div class="profile w-full flex m-3 ml-4 text-white">
-              <img class="w-16 h-16 p-1 bg-white rounded-full" src={on_calls.user[:avatar_url]} alt=""/>
-              <div class="title mt-11 ml-3 font-bold flex flex-col">
+          <div class="card w-full hover:shadow-none relative flex flex-col mx-auto shadow-lg">
+            <div class="profile w-full flex my-3 text-white">
+              <img class="w-12 h-12 p-1 bg-white rounded-full" src={on_calls.user[:avatar_url]} alt=""/>
+              <div class="title ml-3 font-bold flex flex-col">
                 <div class="name break-words"><%= on_calls.user[:name] %></div>
                 <!--  add [dark] class for bright background -->
                 <div class="add font-semibold text-sm italic dark">From: <%= format_date(on_calls[:start]) %>, to: <%= format_date(on_calls[:end]) %></div>
--- lib/live_sup_web/live/widgets/team_members_live.ex
+++ lib/live_sup_web/live/widgets/team_members_live.ex
@@ -12,23 +12,29 @@ defmodule LiveSupWeb.Live.Widgets.TeamMembersLive do
           <%= if Enum.any?(widget_data.data) do %>
           <div class="divide-y divide-gray-100 dark:divide-gray-500">
               <%= for user <- widget_data.data do %>
-                <div class="flex py-4 first:pt-0 gap-4 flex-wrap dark:divide-blue-300">
-                  <div class="flex-none relative">
-                    <img src={user[:avatar]} class="w-14 h-14 rounded-full transition-opacity duration-200 "/>
+                <div class="flex py-4 first:pt-0 gap-3 flex-wrap dark:divide-blue-300">
+                  <div class="flex-none relative pt-1">
+                    <img src={user[:avatar]} class="w-10 h-10 rounded-full transition-opacity duration-200 "/>
                     <%= if user[:night] == true do %>
-                    <i class="absolute bottom-2 right-0 rounded-full bg-slate-700 p-[1px]"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <i class="absolute bottom-0 right-0 rounded-full bg-slate-700 p-[1px]"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
                     </svg></i>
                     <% else %>
-                    <i class="absolute bottom-2 right-0 rounded-full bg-slate-700 p-[1px]"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="rgb(252, 211, 77)">
+                    <i class="absolute bottom-0 right-0 rounded-full bg-slate-700 p-[1px]"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="rgb(252, 211, 77)">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg></i>
                     <% end %>
                   </div>
                   <div class="grow">
                     <p>
-                      <strong><%= user[:full_name] %></strong> <br>
-                      <span class="text-sm"><%= user[:address] %></span> <br>
+                      <strong class="block"><%= user[:full_name] %></strong>
+                      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                      </svg>
+                      <span class="text-sm align-middle inline-block">
+                        <%= user[:address] %>
+                      </span>
+                      |
                       <span class="text-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
