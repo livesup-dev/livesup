19229fb406e6eeb137d54e7572f64360cbdecce5
Memo <guillermo@dinkuminteractive.com>
2022-06-01 19:21:09 +0000

    Add markdown style to notes widget

--- lib/live_sup_web/live/widgets/note_live.ex
+++ lib/live_sup_web/live/widgets/note_live.ex
@@ -8,7 +8,7 @@ defmodule LiveSupWeb.Live.Widgets.NoteLive do
       <!-- Note -->
         <.live_component module={WidgetHeaderComponent} id={"#{widget_data.id}-header"} widget_data={widget_data} />
         <!-- Widget Content -->
-        <div class="items-center p-2 bg-white rounded-md dark:bg-darker min-h-[132px]">
+        <div class="items-center p-2 bg-white rounded-md dark:bg-darker markdown min-h-[132px]">
           <%= raw(widget_data.data[:html_content]) %>
         </div>
         <!-- /Widget Content -->
