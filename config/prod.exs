ed0f9840ea9937451ad76846fa9ffe8f58719c34
mustela <ejankowski@gmail.com>
2023-05-15 22:16:22 +0200

    adding https port

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,13 @@ config :live_sup, LiveSupWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 8080],
   server: true,
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil]
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
+  https: [
+    port: 443
+  ]
 
 # Do not print debug messages in production
 config :logger, level: :info
-
 
 # ## SSL Support
 #
