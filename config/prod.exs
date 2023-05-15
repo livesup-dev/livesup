e59891f0970fbee836f7aacc55c6938f5c7128cc
mustela <ejankowski@gmail.com>
2023-05-15 16:54:27 +0200

    testing ssl

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,11 @@ config :live_sup, LiveSupWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 8080],
   server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil]
 
 # Do not print debug messages in production
 config :logger, level: :info
+
 
 # ## SSL Support
 #
